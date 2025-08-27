f3587da58a458bdaad8552476f4659f2e446a9b4
KunalHS <khotwani@ontic.co>
2025-08-27 14:07:02 +0530

    Changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,31 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- Ctrl+S => save ALL
+map({ "n", "x", "s", "i" }, "<C-s>", "<Cmd>wa<CR><Esc>", { desc = "Save All Files" })
+
+-- jj / jk => Esc (insert mode)
+map("i", "jj", "<Esc>", { desc = "Esc" })
+map("i", "jk", "<Esc>", { desc = "Esc" })
+
+-- <leader>o => keep only current window
+map("n", "<leader>o", "<Cmd>only<CR>", { desc = "Close Other Windows" })
+
+vim.keymap.set("n", "<leader>tt", "<Cmd>ToggleTerm direction=float<CR>", { desc = "Floating Terminal" })
+
+-- <leader>fk => search keymaps
+map("n", "<leader>fk", function()
+  if pcall(require, "snacks") and Snacks.picker and Snacks.picker.keymaps then
+    Snacks.picker.keymaps()
+  else
+    require("telescope.builtin").keymaps()
+  end
+end, { desc = "Keymaps" })
+
+-- <leader>fw => grep word under cursor
+map("n", "<leader>fw", function()
+  if pcall(require, "snacks") and Snacks.picker and Snacks.picker.grep then
+    Snacks.picker.grep({ search = vim.fn.expand("<cword>") })
+  else
+    require("telescope.builtin").grep_string()
+  end
+end, { desc = "Grep Word" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Cursor shapes/colors
+vim.opt.guicursor = {
+  "n-v-c:block", -- block in normal/visual/command
+  "i:hor20-Cursor", -- horizontal bar in insert
+}
+-- Highlight group for the cursor
+vim.cmd([[highlight Cursor guifg=NONE guibg=LightSkyBlue]])
